487bad6a03f23a419cb99d95300ad37ca04d074f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-01 11:29:37 +0100

    feat: add Call to Action section with styled button and image
    - Implemented a responsive CTA section with a headline, description, and button for user engagement.
    - Included a background image and responsive design elements for optimal display on different devices.
    - Integrated SVG image `ctaimg.svg` for visual enhancement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import CaseStudies from './components/CaseStudies'
+import CTA from './components/CTA'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Services from './components/Services'
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <CTA />
+      <CaseStudies />
     </div>
   )
 }
---
+++ src/components/CTA.jsx
@@ -0,0 +1,33 @@
+import CTAImage from '../assets/ctaimg.svg'
+
+const CTA = () => {
+  return (
+    <section className="relative py-12" id="use-cases">
+      <div className="max-w-7xl mx-auto bg-[#f3f3f3] rounded-md sm:pt-24 pt-8 pb-12 flex flex-col  md:flex-row items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+          {' '}
+          <h1 className="text-3xl font-bold text-secondary mb-4">
+            Let&apos;s make things happen
+          </h1>
+          <p className="text-gray-600 mb-6 text-justify md:text-start md:w-2/3">
+            Contact us today to learn more about how our digital marketing
+            services can help your business grow and succeed online.
+          </p>
+          <button className="inline-block px-6  py-3 bg-black text-white font-semibold rounded-md hover:bg-primary hover:text-black transition-all duration-300">
+            Get your free proposal{' '}
+          </button>
+        </div>
+        <div className="md:w-1/2 flex justify-end items-center relative">
+          <img
+            src={CTAImage}
+            alt=""
+            className="md:absolute md:-top-52 md:bottom-0"
+            style={{ transform: `scale(1.05)` }}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CTA
