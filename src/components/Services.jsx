4b9a260f054bff2845a5a7f60e03042fb317e1b4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-01 04:49:12 +0100

    feat: add services section with cards for various offerings
    - Implemented a grid layout for the services section
    - Added individual service cards with titles, icons, and descriptions
    - Applied distinct background colors for each service to enhance visual differentiation

---
+++ src/components/Services.jsx
@@ -0,0 +1,56 @@
+import { SERVICES } from '../constants'
+import LinkIcon from '../assets/Icon.svg'
+
+const Services = () => {
+  return (
+    <section className="pt-24 pb-16 bg-white" id="services">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row gap-4 mb-12">
+          <div className="flex-shrink-0 bg-primary text-black py-2 px-16 rounded-md">
+            <h2 className="text-2xl font-bold">Services</h2>
+          </div>
+          <div className="">
+            <p className="text-secondary md:w-2/3">
+              At our digital marketing agency, we offer a range of services to
+              help business grow and succeed online. These services include:
+            </p>
+          </div>
+        </div>
+        <div className="grid grod-cols-1 md:grid-cols-2 gap-6">
+          {SERVICES.map((service, index) => (
+            <div
+              className={`flex flex-col md:flex-row md:justify-between md:items-center gap-6 p-6 rounded-lg cursor-pointer border border-[#191a23] ${service.backgroundColor} ${service.textColor}`}
+              key={index}
+            >
+              <div className="md:w-1/2 flex flex-col h-full justify-between">
+                <button
+                  className={`text-3xl py-1.5 font-semibold mb-2 rounded-sm sm:w-60 ${service.foregroundColor}`}
+                >
+                  {service.title}
+                </button>
+
+                <a
+                  href="#"
+                  className="flex items-center gap-4 py-2 rounded-md text-black hover:bg-opacity-80 hover:text-black/80"
+                >
+                  <img src={LinkIcon} alt="" className="size-6 rounded-full" />
+                  <span className="text-lg font-medium">Learn more</span>
+                </a>
+              </div>
+
+              <div className="md:w-1/2">
+                <img
+                  src={service.image}
+                  alt=""
+                  className="w-full object-cover rounded-md mb-4"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,12 @@ import hubSpotLogo from '../assets/hubspot.svg'
 import notionLogo from '../assets/notion.svg'
 import netFlixLogo from '../assets/netflix.svg'
 import zoomLogo from '../assets/zoom.svg'
+import seoImg from '../assets/seo.svg'
+import payperclickImg from '../assets/payperclick.svg'
+import socialmarketingImg from '../assets/socialmediamarketing.svg'
+import emailmarketing from '../assets/emailmarketing.svg'
+import contentcreationImg from '../assets/contentcreation.svg'
+import analyticsImg from '../assets/analysis.svg'
 
 export const NAV_MENU_ITEMS = [
   {
@@ -35,4 +41,58 @@ export const BRAND_LOGOS = [
   notionLogo,
   netFlixLogo,
   zoomLogo,
+]
+
+export const SERVICES = [
+  {
+    title: 'Search engine optimization',
+    description:
+      'Improve your website’s visibility on search engines with our SEO services.',
+    image: seoImg,
+    backgroundColor: 'bg-[#f3f3f3]',
+    foregroundColor: 'bg-primary',
+    textColor: 'text-black',
+  },
+  {
+    title: 'Pay-per-click Advertising',
+    description:
+      'Boost your online presence with targeted PPC advertising campaigns.',
+    image: payperclickImg,
+    backgroundColor: 'bg-primary',
+    foregroundColor: 'bg-white',
+    textColor: 'text-blue-900',
+  },
+  {
+    title: 'Social Media Marketing',
+    description:
+      'Engage your audience and grow your brand on social media platforms.',
+    image: socialmarketingImg,
+    backgroundColor: 'bg-green-100',
+    foregroundColor: 'bg-green-500',
+    textColor: 'text-green-900',
+  },
+  {
+    title: 'Email Marketing',
+    description:
+      'Reach your audience directly with targeted email campaigns and newsletters.',
+    image: emailmarketing,
+  },
+  {
+    title: 'Content Creation',
+    description:
+      'Create compelling content that drives engagement and conversions.',
+    image: contentcreationImg,
+    backgroundColor: 'bg-red-100',
+    foregroundColor: 'bg-red-500',
+    textColor: 'text-red-900',
+  },
+  {
+    title: 'Analytics and Tracking',
+    description:
+      'Track and analyze your website’s performance to make data-driven decisions.',
+    image: analyticsImg,
+    backgroundColor: 'bg-gray-100',
+    foregroundColor: 'bg-primary',
+    textColor: 'text-gray-900',
+  },
 ]
